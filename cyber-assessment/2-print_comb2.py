71f4ab5015b4c3d3afcbcbc4f11bf059e8c2b333
Lucas Hillesheim <lucashillesheim11@gmail.com>
2026-02-18 12:58:26 +0000

    Task 3: 00...99

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0,100):
-    if i == 99:
-        print(f'{i}')
-    else:
-        print(f'{i:02}, ', end='')
+print(''.join(str(f'{x:02}, ') for x in range(0,99)), end='')
+print('99\n')
+
